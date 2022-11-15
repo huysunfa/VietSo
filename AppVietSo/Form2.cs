4db3f62a664584bd80669ddc5c84eecc7ea3434f
Huy1965 <Huy1965@DESKTOP-Q70KEUJ>
2022-11-15 09:20:56 +0700

    Sửa lỗi phần in

--- AppVietSo/Form2.cs
+++ AppVietSo/Form2.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using AppVietSo.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,27 +26,24 @@ namespace AppVietSo
             var sheet = reoGridControl1.CurrentWorksheet;
             var row = sheet.UsedRange.Row;
             var col = sheet.UsedRange.Row;
-            sheet.SetCols(10);
-            sheet.SetRows(10);
-         
+            sheet.SetCols(32);
+            sheet.SetRows(32);
 
-            sheet.SetRowsHeight(0, 10, 30);
-            sheet.SetColumnsWidth(0, 10, 59);
-            sheet.ResetAllPageBreaks();
-            sheet.SetRangeStyles(sheet.UsedRange, new unvell.ReoGrid.WorksheetRangeStyle
+            sheet.SetRowsHeight(0, 32, 30);
+            sheet.SetColumnsWidth(0, 32, 59);
+
+            for (int i = 0; i < sheet.UsedRange.Rows; i++)
             {
-                Flag = unvell.ReoGrid.PlainStyleFlag.FontSize,
-                FontSize = 26
-            });
-           sheet.SetSettings(WorksheetSettings.View_ShowHeaders, false);
-          // sheet.SetSettings(WorksheetSettings.Behavior_MouseWheelToScroll, false);
-          // sheet.SetSettings(WorksheetSettings.Behavior_ScrollToFocusCell, false);
-          // sheet.SetSettings(WorksheetSettings.Edit_AutoExpandColumnWidth, false);
-          sheet.SetSettings(WorksheetSettings.Edit_AllowAdjustColumnWidth, true);
-           sheet.SetSettings(WorksheetSettings.Edit_AutoExpandRowHeight, true);
-           sheet.SetSettings(WorksheetSettings.View_AllowShowOutlines, true);
-           sheet.SetSettings(WorksheetSettings.View_AllowCellTextOverflow, true);
-            sheet.SetSettings(WorksheetSettings.View_ShowGridLine, true);
+
+                for (int j = 0; j < sheet.UsedRange.Cols; j++)
+                {
+                    sheet.Cells[i, j].Data = i * j;
+                }
+            }
+          
+ 
+            sheet.SetSettings(WorksheetSettings.View_ShowPageBreaks, true);
+     
         }
 
         private bool m_bLayoutCalled = false;
@@ -60,15 +59,34 @@ namespace AppVietSo
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var worksheet = reoGridControl1.CurrentWorksheet;
-            var cnt = worksheet.SelectionRange.Col;
-            var action = new InsertColumnsAction(cnt, 1);
-            reoGridControl1.DoAction(action);
+            System.Drawing.Printing.PrintDocument doc = null;
 
-            for (int i = 0; i < worksheet.UsedRange.EndRow; i++)
+            try
             {
-                worksheet.Cells[i, cnt].Data = "頂";
+                doc = reoGridControl1.CurrentWorksheet.CreatePrintSession().PrintDocument;
             }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, this.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            using (var pd = new System.Windows.Forms.PrintDialog())
+            {
+                pd.Document = doc;
+                pd.UseEXDialog = true;  // in 64bit Windows
+
+                if (pd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    
+                }
+            }
+
+            if (doc != null) doc.Dispose();
+
         }
+
+     
+
     }
 }
